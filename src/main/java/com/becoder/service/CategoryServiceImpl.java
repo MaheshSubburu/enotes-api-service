75f953aeae1196b0cc3f01069cc485ca8c0384cd
MaheshSubburu <maheshbhaisubburu123@gmail.com>
2026-04-14 21:33:31 +0530

    Dto Mapper and Response
    Dto Mapper and Response

--- src/main/java/com/becoder/service/CategoryServiceImpl.java
+++ src/main/java/com/becoder/service/CategoryServiceImpl.java
@@ -2,10 +2,13 @@ package com.becoder.service;
 
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
+import com.becoder.dto.CategoryDto;
+import com.becoder.dto.CategoryResponse;
 import com.becoder.entity.Category;
 import com.becoder.repository.CategoryRepository;
 
@@ -14,20 +17,45 @@ public class CategoryServiceImpl implements CategoryService {
 	
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ModelMapper modelMapper;
 
 	@Override
-	public Boolean saveCategory(Category category) {
-		category.setIsDeleted(false);
-		category.setCreatedBy(1);
+	public Boolean saveCategory(CategoryDto categoryDto) {
+		
+//		Category category = new Category();
+//		category.setName(categoryDto.getName());
+//		category.setDescription(categoryDto.getDescription());
+//		category.setIsActive(categoryDto.getIsActive());
+		
+		Category category = modelMapper.map(categoryDto, Category.class);
+		
+		categoryDto.setIsDeleted(false);
+		categoryDto.setCreatedBy(1);
+		
 		Category saveCategory = categoryRepository.save(category);
 		if(ObjectUtils.isEmpty(saveCategory)) return false;
 		return true;
 	}
 
 	@Override
-	public List<Category> getAllCategory() {
+	public List<CategoryDto> getAllCategory() {
 		List<Category> categories = categoryRepository.findAll();
-		return categories;
+		
+		List<CategoryDto> categoryDtoList = categories.stream().map(cat -> modelMapper.map(categories, CategoryDto.class)).toList();
+		return categoryDtoList;
+	}
+
+	@Override
+	public List<CategoryResponse> getActiveCategory() {
+		
+		List<Category> categories = categoryRepository.findByIsActiveTrue();
+		
+		List<CategoryResponse> categoryResponseList = 
+				categories.stream().map(cat -> modelMapper.map(cat, CategoryResponse.class)).toList();
+		
+		return categoryResponseList;
 	}
 	
 	
