b44c44c2c83594cb3487d47087cc09db8b08bf99
MaheshSubburu <maheshbhaisubburu123@gmail.com>
2026-05-10 11:58:34 +0530

    Last Commit
    Last commit.

--- src/main/java/com/becoder/entity/BaseModel.java
+++ src/main/java/com/becoder/entity/BaseModel.java
@@ -2,15 +2,20 @@ package com.becoder.entity;
 
 import java.util.Date;
 
+import jakarta.persistence.Access;
+import jakarta.persistence.AccessType;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Transient;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Access(AccessType.FIELD)
 @MappedSuperclass
 public class BaseModel {
 
+	@Transient
     private Boolean isActive;
 	
 	private Boolean isDeleted;
